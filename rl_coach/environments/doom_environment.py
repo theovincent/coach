89ea37353bb57985e01dcf6ffdb7a5494bd6da3b
Théo <theo.vincent@eleves.enpc.fr>
2021-12-27 15:55:45 +0000

    Prepare for training with depth

--- rl_coach/environments/doom_environment.py
+++ rl_coach/environments/doom_environment.py
@@ -18,6 +18,7 @@ try:
     import vizdoom
 except ImportError:
     from rl_coach.logger import failed_imports
+
     failed_imports.append("ViZDoom")
 
 import os
@@ -36,8 +37,7 @@ from rl_coach.filters.observation.observation_rescale_to_size_filter import Obse
 from rl_coach.filters.observation.observation_rgb_to_y_filter import ObservationRGBToYFilter
 from rl_coach.filters.observation.observation_stacking_filter import ObservationStackingFilter
 from rl_coach.filters.observation.observation_to_uint8_filter import ObservationToUInt8Filter
-from rl_coach.spaces import MultiSelectActionSpace, ImageObservationSpace, \
-    VectorObservationSpace, StateSpace
+from rl_coach.spaces import MultiSelectActionSpace, ImageObservationSpace, VectorObservationSpace, StateSpace
 
 
 # enum of the available levels and their path
@@ -48,60 +48,70 @@ class DoomLevel(Enum):
     MY_WAY_HOME = "my_way_home.cfg"
     TAKE_COVER = "take_cover.cfg"
     HEALTH_GATHERING = "health_gathering.cfg"
-    HEALTH_GATHERING_SUPREME_COACH_LOCAL = "D2_navigation.cfg"  # from https://github.com/IntelVCL/DirectFuturePrediction/tree/master/maps
+    HEALTH_GATHERING_SUPREME_COACH_LOCAL = (
+        "D2_navigation.cfg"  # from https://github.com/IntelVCL/DirectFuturePrediction/tree/master/maps
+    )
     DEFEND_THE_LINE = "defend_the_line.cfg"
     DEADLY_CORRIDOR = "deadly_corridor.cfg"
     BATTLE_COACH_LOCAL = "D3_battle.cfg"  # from https://github.com/IntelVCL/DirectFuturePrediction/tree/master/maps
 
+
 key_map = {
-    'NO-OP': 96,  # `
-    'ATTACK': 13,  # enter
-    'CROUCH': 306,  # ctrl
-    'DROP_SELECTED_ITEM': ord("t"),
-    'DROP_SELECTED_WEAPON': ord("t"),
-    'JUMP': 32,  # spacebar
-    'LAND': ord("l"),
-    'LOOK_DOWN': 274,  # down arrow
-    'LOOK_UP': 273,  # up arrow
-    'MOVE_BACKWARD': ord("s"),
-    'MOVE_DOWN': ord("s"),
-    'MOVE_FORWARD': ord("w"),
-    'MOVE_LEFT': 276,
-    'MOVE_RIGHT': 275,
-    'MOVE_UP': ord("w"),
-    'RELOAD': ord("r"),
-    'SELECT_NEXT_WEAPON': ord("q"),
-    'SELECT_PREV_WEAPON': ord("e"),
-    'SELECT_WEAPON0': ord("0"),
-    'SELECT_WEAPON1': ord("1"),
-    'SELECT_WEAPON2': ord("2"),
-    'SELECT_WEAPON3': ord("3"),
-    'SELECT_WEAPON4': ord("4"),
-    'SELECT_WEAPON5': ord("5"),
-    'SELECT_WEAPON6': ord("6"),
-    'SELECT_WEAPON7': ord("7"),
-    'SELECT_WEAPON8': ord("8"),
-    'SELECT_WEAPON9': ord("9"),
-    'SPEED': 304,  # shift
-    'STRAFE': 9,  # tab
-    'TURN180': ord("u"),
-    'TURN_LEFT': ord("a"),  # left arrow
-    'TURN_RIGHT': ord("d"),  # right arrow
-    'USE': ord("f"),
+    "NO-OP": 96,  # `
+    "ATTACK": 13,  # enter
+    "CROUCH": 306,  # ctrl
+    "DROP_SELECTED_ITEM": ord("t"),
+    "DROP_SELECTED_WEAPON": ord("t"),
+    "JUMP": 32,  # spacebar
+    "LAND": ord("l"),
+    "LOOK_DOWN": 274,  # down arrow
+    "LOOK_UP": 273,  # up arrow
+    "MOVE_BACKWARD": ord("s"),
+    "MOVE_DOWN": ord("s"),
+    "MOVE_FORWARD": ord("w"),
+    "MOVE_LEFT": 276,
+    "MOVE_RIGHT": 275,
+    "MOVE_UP": ord("w"),
+    "RELOAD": ord("r"),
+    "SELECT_NEXT_WEAPON": ord("q"),
+    "SELECT_PREV_WEAPON": ord("e"),
+    "SELECT_WEAPON0": ord("0"),
+    "SELECT_WEAPON1": ord("1"),
+    "SELECT_WEAPON2": ord("2"),
+    "SELECT_WEAPON3": ord("3"),
+    "SELECT_WEAPON4": ord("4"),
+    "SELECT_WEAPON5": ord("5"),
+    "SELECT_WEAPON6": ord("6"),
+    "SELECT_WEAPON7": ord("7"),
+    "SELECT_WEAPON8": ord("8"),
+    "SELECT_WEAPON9": ord("9"),
+    "SPEED": 304,  # shift
+    "STRAFE": 9,  # tab
+    "TURN180": ord("u"),
+    "TURN_LEFT": ord("a"),  # left arrow
+    "TURN_RIGHT": ord("d"),  # right arrow
+    "USE": ord("f"),
 }
 
 
 DoomInputFilter = InputFilter(is_a_reference_filter=True)
-DoomInputFilter.add_observation_filter('observation', 'rescaling',
-                                       ObservationRescaleToSizeFilter(ImageObservationSpace(np.array([60, 76, 3]),
-                                                                                            high=255)))
-DoomInputFilter.add_observation_filter('observation', 'to_grayscale', ObservationRGBToYFilter())
-DoomInputFilter.add_observation_filter('observation', 'to_uint8', ObservationToUInt8Filter(0, 255))
-DoomInputFilter.add_observation_filter('observation', 'stacking', ObservationStackingFilter(3))
+DoomInputFilter.add_observation_filter(
+    "observation", "rescaling", ObservationRescaleToSizeFilter(ImageObservationSpace(np.array([60, 76, 3]), high=255))
+)
+DoomInputFilter.add_observation_filter("observation", "to_grayscale", ObservationRGBToYFilter())
+DoomInputFilter.add_observation_filter("observation", "to_uint8", ObservationToUInt8Filter(0, 255))
+DoomInputFilter.add_observation_filter("observation", "stacking", ObservationStackingFilter(3))
+
+DoomInputFilter.add_observation_filter(
+    "depth", "rescaling", ObservationRescaleToSizeFilter(ImageObservationSpace(np.array([60, 76, 3]), high=255))
+)
+DoomInputFilter.add_observation_filter("depth", "to_grayscale", ObservationRGBToYFilter())
+DoomInputFilter.add_observation_filter("depth", "to_uint8", ObservationToUInt8Filter(0, 255))
+DoomInputFilter.add_observation_filter("depth", "stacking", ObservationStackingFilter(3))
 
 
 DoomOutputFilter = OutputFilter(is_a_reference_filter=True)
-DoomOutputFilter.add_action_filter('to_discrete', FullDiscreteActionSpaceMap())
+DoomOutputFilter.add_action_filter("to_discrete", FullDiscreteActionSpaceMap())
 
 
 class DoomEnvironmentParameters(EnvironmentParameters):
@@ -109,11 +119,11 @@ class DoomEnvironmentParameters(EnvironmentParameters):
         super().__init__(level=level)
         self.default_input_filter = DoomInputFilter
         self.default_output_filter = DoomOutputFilter
-        self.cameras = [DoomEnvironment.CameraTypes.OBSERVATION, DoomEnvironment.CameraTypes.DEPTH, DoomEnvironment.CameraTypes.LABELS, DoomEnvironment.CameraTypes.MAP]
+        self.cameras = [DoomEnvironment.CameraTypes.OBSERVATION, DoomEnvironment.CameraTypes.DEPTH]
 
     @property
     def path(self):
-        return 'rl_coach.environments.doom_environment:DoomEnvironment'
+        return "rl_coach.environments.doom_environment:DoomEnvironment"
 
 
 class DoomEnvironment(Environment):
@@ -123,9 +133,18 @@ class DoomEnvironment(Environment):
         LABELS = ("labels", "labels_buffer")
         MAP = ("map", "automap_buffer")
 
-    def __init__(self, level: LevelSelection, seed: int, frame_skip: int, human_control: bool,
-                 custom_reward_threshold: Union[int, float], visualization_parameters: VisualizationParameters,
-                 cameras: List[CameraTypes], target_success_rate: float=1.0, **kwargs):
+    def __init__(
+        self,
+        level: LevelSelection,
+        seed: int,
+        frame_skip: int,
+        human_control: bool,
+        custom_reward_threshold: Union[int, float],
+        visualization_parameters: VisualizationParameters,
+        cameras: List[CameraTypes],
+        target_success_rate: float = 1.0,
+        **kwargs,
+    ):
         """
         :param level: (str)
             A string representing the doom level to run. This can also be a LevelSelection object.
@@ -156,19 +175,27 @@ class DoomEnvironment(Environment):
             Stop experiment if given target success rate was achieved.
 
         """
-        super().__init__(level, seed, frame_skip, human_control, custom_reward_threshold, visualization_parameters, target_success_rate)
+        super().__init__(
+            level,
+            seed,
+            frame_skip,
+            human_control,
+            custom_reward_threshold,
+            visualization_parameters,
+            target_success_rate,
+        )
 
         self.cameras = cameras
 
         # load the emulator with the required level
         self.level = DoomLevel[level.upper()]
-        local_scenarios_path = path.join(os.path.dirname(os.path.realpath(__file__)), 'doom')
-        if 'COACH_LOCAL' in level:
+        local_scenarios_path = path.join(os.path.dirname(os.path.realpath(__file__)), "doom")
+        if "COACH_LOCAL" in level:
             self.scenarios_dir = local_scenarios_path
-        elif 'VIZDOOM_ROOT' in environ:
-            self.scenarios_dir = path.join(environ.get('VIZDOOM_ROOT'), 'scenarios')
+        elif "VIZDOOM_ROOT" in environ:
+            self.scenarios_dir = path.join(environ.get("VIZDOOM_ROOT"), "scenarios")
         else:
-            self.scenarios_dir = path.join(os.path.dirname(os.path.realpath(vizdoom.__file__)), 'scenarios')
+            self.scenarios_dir = path.join(os.path.dirname(os.path.realpath(vizdoom.__file__)), "scenarios")
 
         self.game = vizdoom.DoomGame()
         self.game.load_config(path.join(self.scenarios_dir, self.level.value))
@@ -189,18 +216,20 @@ class DoomEnvironment(Environment):
         self.game.set_render_decals(False)
         self.game.set_render_particles(False)
         for camera in self.cameras:
-            if hasattr(self.game, 'set_{}_enabled'.format(camera.value[1])):
-                getattr(self.game, 'set_{}_enabled'.format(camera.value[1]))(True)
+            if hasattr(self.game, "set_{}_enabled".format(camera.value[1])):
+                getattr(self.game, "set_{}_enabled".format(camera.value[1]))(True)
         self.game.init()
 
         # actions
-        actions_description = ['NO-OP']
+        actions_description = ["NO-OP"]
         actions_description += [str(action).split(".")[1] for action in self.game.get_available_buttons()]
         actions_description = actions_description[::-1]
-        self.action_space = MultiSelectActionSpace(self.game.get_available_buttons_size(),
-                                                   max_simultaneous_selected_actions=1,
-                                                   descriptions=actions_description,
-                                                   allow_no_action_to_be_selected=True)
+        self.action_space = MultiSelectActionSpace(
+            self.game.get_available_buttons_size(),
+            max_simultaneous_selected_actions=1,
+            descriptions=actions_description,
+            allow_no_action_to_be_selected=True,
+        )
 
         # human control
         if self.human_control:
@@ -211,15 +240,18 @@ class DoomEnvironment(Environment):
                     self.key_to_action[(key_map[action],)] = idx
 
         # states
-        self.state_space = StateSpace({
-            "measurements": VectorObservationSpace(self.game.get_state().game_variables.shape[0],
-                                                   measurements_names=[str(m) for m in
-                                                                       self.game.get_available_game_variables()])
-        })
+        self.state_space = StateSpace(
+            {
+                "measurements": VectorObservationSpace(
+                    self.game.get_state().game_variables.shape[0],
+                    measurements_names=[str(m) for m in self.game.get_available_game_variables()],
+                )
+            }
+        )
         for camera in self.cameras:
             self.state_space[camera.value[0]] = ImageObservationSpace(
-                shape=np.array([self.game.get_screen_height(), self.game.get_screen_width(), 3]),
-                high=255)
+                shape=np.array([self.game.get_screen_height(), self.game.get_screen_width(), 3]), high=255
+            )
 
         # seed
         if seed is not None:
@@ -238,7 +270,7 @@ class DoomEnvironment(Environment):
         state = self.game.get_state()
         if state is not None and state.screen_buffer is not None:
             self.measurements = state.game_variables
-            self.state = {'measurements': self.measurements}
+            self.state = {"measurements": self.measurements}
             for camera in self.cameras:
                 observation = getattr(state, camera.value[1])
                 if len(observation.shape) == 3:
@@ -263,11 +295,13 @@ class DoomEnvironment(Environment):
         """
         image = [self.state[camera.value[0]] for camera in self.cameras]
         image = np.vstack(image)
-        
-        pil_image = Image.fromarray(image)        
+
+        pil_image = Image.fromarray(image)
         drawer = ImageDraw.Draw(pil_image)
-        
-        drawer.text((10,10), f"Health :{self.state['measurements'][0]}", fill=(255,255,0))
+        drawer.text((10, 10), f"Health :{self.state['measurements'][0]}", fill=(255, 255, 0))
+        drawer.text(
+            (self.game.get_screen_width() - 100, 10), f"Step :{self.current_episode_steps_counter}", fill=(255, 255, 0)
+        )
 
         return np.array(pil_image)
 
--- rl_coach/presets/Doom_Health_DFP.py
+++ rl_coach/presets/Doom_Health_DFP.py
@@ -1,6 +1,10 @@
 from rl_coach.agents.dfp_agent import DFPAgentParameters
-from rl_coach.base_parameters import VisualizationParameters, EmbedderScheme, MiddlewareScheme, \
-    PresetValidationParameters
+from rl_coach.base_parameters import (
+    VisualizationParameters,
+    EmbedderScheme,
+    MiddlewareScheme,
+    PresetValidationParameters,
+)
 from rl_coach.core_types import EnvironmentSteps, EnvironmentEpisodes
 from rl_coach.environments.doom_environment import DoomEnvironmentParameters
 from rl_coach.graph_managers.basic_rl_graph_manager import BasicRLGraphManager
@@ -12,7 +16,8 @@ from rl_coach.schedules import LinearSchedule
 ####################
 
 schedule_params = ScheduleParameters()
-schedule_params.improve_steps = EnvironmentSteps(6250000)
+# schedule_params.improve_steps = EnvironmentSteps(6250000)
+schedule_params.improve_steps = EnvironmentSteps(625)
 # original paper evaluates according to these. But, this preset converges significantly faster - can be evaluated
 # much often.
 # schedule_params.steps_between_evaluation_periods = EnvironmentSteps(62500)
@@ -26,9 +31,9 @@ schedule_params.evaluation_steps = EnvironmentEpisodes(1)
 # Agent #
 #########
 agent_params = DFPAgentParameters()
-schedule_params.heatup_steps = EnvironmentSteps(agent_params.network_wrappers['main'].batch_size)
+schedule_params.heatup_steps = EnvironmentSteps(agent_params.network_wrappers["main"].batch_size)
 
-agent_params.network_wrappers['main'].learning_rate = 0.0001
+agent_params.network_wrappers["main"].learning_rate = 0.0001
 agent_params.exploration.epsilon_schedule = LinearSchedule(0.5, 0, 10000)
 agent_params.exploration.evaluation_epsilon = 0
 agent_params.algorithm.goal_vector = [1]  # health
@@ -37,23 +42,23 @@ agent_params.algorithm.goal_vector = [1]  # health
 agent_params.algorithm.num_consecutive_playing_steps = EnvironmentSteps(1)
 
 # scale observation and measurements to be -0.5 <-> 0.5
-agent_params.network_wrappers['main'].input_embedders_parameters['measurements'].input_rescaling['vector'] = 100.
-agent_params.network_wrappers['main'].input_embedders_parameters['measurements'].input_offset['vector'] = 0.5
-agent_params.network_wrappers['main'].input_embedders_parameters['observation'].input_offset['vector'] = 0.5
+agent_params.network_wrappers["main"].input_embedders_parameters["measurements"].input_rescaling["vector"] = 100.0
+agent_params.network_wrappers["main"].input_embedders_parameters["measurements"].input_offset["vector"] = 0.5
 
 # changing the network scheme to match Coach's default network, as it performs better on this preset
-agent_params.network_wrappers['main'].input_embedders_parameters['observation'].scheme = EmbedderScheme.Medium
-agent_params.network_wrappers['main'].input_embedders_parameters['measurements'].scheme = EmbedderScheme.Medium
-agent_params.network_wrappers['main'].input_embedders_parameters['goal'].scheme = EmbedderScheme.Medium
-agent_params.network_wrappers['main'].middleware_parameters.scheme = MiddlewareScheme.Medium
+agent_params.network_wrappers["main"].input_embedders_parameters["observation"].scheme = EmbedderScheme.Medium
+agent_params.network_wrappers["main"].input_embedders_parameters["depth"].scheme = EmbedderScheme.Medium
+agent_params.network_wrappers["main"].input_embedders_parameters["measurements"].scheme = EmbedderScheme.Empty
+agent_params.network_wrappers["main"].input_embedders_parameters["goal"].scheme = EmbedderScheme.Empty
+agent_params.network_wrappers["main"].middleware_parameters.scheme = MiddlewareScheme.Medium
 
 # scale the target measurements according to the paper (dividing by standard deviation)
-agent_params.algorithm.scale_measurements_targets['GameVariable.HEALTH'] = 30.0
+agent_params.algorithm.scale_measurements_targets["GameVariable.HEALTH"] = 30.0
 
 ###############
 # Environment #
 ###############
-env_params = DoomEnvironmentParameters(level='HEALTH_GATHERING')
+env_params = DoomEnvironmentParameters(level="HEALTH_GATHERING")
 
 
 ########
@@ -66,6 +71,10 @@ preset_validation_params.min_reward_threshold = 1000
 preset_validation_params.max_episodes_to_achieve_reward = 70
 preset_validation_params.test_using_a_trace_test = False
 
-graph_manager = BasicRLGraphManager(agent_params=agent_params, env_params=env_params,
-                                    schedule_params=schedule_params, vis_params=VisualizationParameters(),
-                                    preset_validation_params=preset_validation_params)
+graph_manager = BasicRLGraphManager(
+    agent_params=agent_params,
+    env_params=env_params,
+    schedule_params=schedule_params,
+    vis_params=VisualizationParameters(),
+    preset_validation_params=preset_validation_params,
+)
