75a975ed19b43ffd09558b06e2ded18153989c31
Théo <theo.vincent@eleves.enpc.fr>
2022-01-10 10:27:31 +0000

    Store command lines

--- launch_coach_scripts.sh
+++ launch_coach_scripts.sh
@@ -1,3 +1,7 @@
 # This file is a cheat shit, it is not meant to be executed.
 
 coach -p Doom_Health_Supreme_NoSpawn_DFP -e trash -r --evaluate -crd experiments/cpu_training_health_supreme_depth/27_12_2021-17_28/checkpoint/
+
+coach -p Doom_Health_Supreme_DFP -n 1 -s 1800 -e cpu_training_health_supreme_depth_maze_from_depth -crd experiments/cpu_training_health_supreme_depth/27_12_2021-17_28/checkpoint/
+
+dashboard -d experiments/cpu_training_health_supreme_depth_maze/28_12_2021-17_11/ -rc
